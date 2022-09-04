bdcb0366fd17abe3c9dfb01834cff550c0c57151
Hakuyo Harimoto <haharimoto@gmail.com>
2022-09-05 08:37:24 +0900

    instagram api stopped working after uploading to public github repo, used some fav photos instead

--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 (function ($) {
-  $.ajax({  // jQueryのajaxでjsonデータを取得しますね
+  $.ajax({  // jQueryのajaxでjsonデータを取得
       type: 'GET',
-      url: 'https://graph.facebook.com/17841401582209572?access_token=EAAKH4KhSkyEBAC8VUkL0CDrZAq5bWjWepkw68KBog7ePTEamhlhF5sVqZAa9ZB8xxZA6lzaXAl7KGnjKCFHHJQy4bR7l1YyjWrNEmpCCOIDqyMKJXVcgEFH7pkFG8WHbfBx8tPTimOn9iOgrtzyzrgJRZBwgZC4ZBNovzm9VhfGqMDeAm103BAT&fields=name,media{caption,like_count,media_url,permalink,timestamp,username}',
+      url: '',
       dataType: 'json',
       success: function (json) {
           const insta = json.media.data;
@@ -10,7 +10,7 @@
             let href = insta[i].permalink; // リンク先URLを取得
             let caption = insta[i].caption; //　投稿のキャプションを取得
             let like = insta[i].like_count; //　いいね！数の取得
-            if(url.indexOf('.mp4') <= 0){ // 今回は動画は除外させました .mp4以外を<li>タグで描画します
+            if(url.indexOf('.mp4') <= 0){ // 今回は動画は除外 .mp4以外を<li>タグで描画
               $('.insta_list').append(`
                 <a href="${href}" target="_blank">
                   <img src="${url}">
@@ -21,12 +21,3 @@
       }
   });
 })(jQuery);
-
-
-/* <li>
-<a href="${href}" target="_blank">
-  <img src="${url}" alt="${caption}">
-  <p>${caption}</p>
-  <p>💖<span>${like}</span></p>
-</a>
-</li> */
